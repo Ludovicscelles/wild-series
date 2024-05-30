f9061e10c8ead34d3b2fc40076b69706231f5bac
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-05-30 21:23:39 +0200

    06_Create_your_first_repository

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,30 +1,38 @@
+const tables = require("../../database/tables");
+
 // Some data to make the trick
 
 const categories = [
-    {
-      id: 1,
-      name: "Science-Fiction",
-    },
-    {
-      id: 2,
-      name: "Comédie",
-    },
-  ];
+  {
+    id: 1,
+    name: "Science-Fiction",
+  },
+  {
+    id: 2,
+    name: "Comédie",
+  },
+];
 
-  const browse = (req, res) => {
-    res.json(categories);
-  };
-  
-  const read = (req, res) => {
-    const parsedId = parseInt(req.params.id, 10);
-  
-    const program = categories.find((p) => p.id === parsedId);
-  
-    if (program != null) {
-      res.json(program);
-    } else {
-      res.sendStatus(404);
-    }
-  };
-  
-  module.exports = { browse, read };
+// Declare the actions
+
+const browse = async (req, res) => {
+  const categoriesFromDB = await tables.category.readAll();
+
+  res.json(categoriesFromDB);
+};
+
+const read = (req, res) => {
+  const parsedId = parseInt(req.params.id, 10);
+
+  const category = categories.find((p) => p.id === parsedId);
+
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+// Export them to import them somewhere else
+
+module.exports = { browse, read };
